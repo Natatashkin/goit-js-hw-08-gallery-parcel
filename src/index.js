4b699eb9dc8ba69e62dead44b43eb78f33ee9f76
Natatashkin <cs070283sng@gmail.com>
2020-10-18 16:25:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./css/style.css";
 import images from "./js/gallery-items.js";
+import { createListItem, onImageClick, onModalOpen, onChangeLightboxImageUrl, getIndex, setLightboxImageRefAttribute, onShowNextImage, onShowPreviousImage, onCloseModal, onCloseModalByEsc, onCloseLightboxByClick, onClearlightboxImageRef} from './js/script';
 
 const galleryRef = document.querySelector(".js-gallery");
 const lightboxRef = document.querySelector(".js-lightbox");
@@ -17,114 +18,114 @@ lightboxOverlay.addEventListener("click", onCloseLightboxByClick);
 lightBoxCloseBtn.addEventListener("click", onCloseModal);
 
 
-function createListItem (galleryItems) {
-  return galleryItems.map(({ preview, original, description }) => {
-    return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img class="gallery__image"
-          src="${preview}" 
-          data-source="${original}" 
-          alt="${description}"/>
-      </a>
-    </li >
-    `
-  }).join("");
-};
+// function createListItem (galleryItems) {
+//   return galleryItems.map(({ preview, original, description }) => {
+//     return `
+//     <li class="gallery__item">
+//       <a class="gallery__link" href="${original}">
+//         <img class="gallery__image"
+//           src="${preview}" 
+//           data-source="${original}" 
+//           alt="${description}"/>
+//       </a>
+//     </li >
+//     `
+//   }).join("");
+// };
 
-function onImageClick(event) {
-  event.preventDefault();
-  return event.target.dataset.source;
-}
+// function onImageClick(event) {
+//   event.preventDefault();
+//   return event.target.dataset.source;
+// }
 
-function onModalOpen(event) {
-  event.preventDefault();
-  window.addEventListener("keydown", onCloseModalByEsc);
-  window.addEventListener("keydown", onShowNextImage);
-   window.addEventListener("keydown", onShowPreviousImage);
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
+// function onModalOpen(event) {
+//   event.preventDefault();
+//   window.addEventListener("keydown", onCloseModalByEsc);
+//   window.addEventListener("keydown", onShowNextImage);
+//    window.addEventListener("keydown", onShowPreviousImage);
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
 
-  const currentActiveImage = document.querySelector(".is-open");
-  if (currentActiveImage) {
-    lightboxRef.classList.remove("is-open");
-  }
+//   const currentActiveImage = document.querySelector(".is-open");
+//   if (currentActiveImage) {
+//     lightboxRef.classList.remove("is-open");
+//   }
 
-  lightboxRef.classList.add("is-open");
-  onChangeLightboxImageUrl(event);
-}
+//   lightboxRef.classList.add("is-open");
+//   onChangeLightboxImageUrl(event);
+// }
  
-function onChangeLightboxImageUrl(event) {
-  if (lightboxRef.classList.contains("is-open")) {
-    lightboxImageRef.src = onImageClick(event);
-    lightboxImageRef.alt = event.target.alt;
-  }
-}
+// function onChangeLightboxImageUrl(event) {
+//   if (lightboxRef.classList.contains("is-open")) {
+//     lightboxImageRef.src = onImageClick(event);
+//     lightboxImageRef.alt = event.target.alt;
+//   }
+// }
 
- function getIndex() {
-    return images.findIndex(elem => lightboxImageRef.src === elem.original);
-}
+//  function getIndex() {
+//     return images.findIndex(elem => lightboxImageRef.src === elem.original);
+// }
 
-function setLightboxImageRefAttribute(index) {
-  const nextImageSrc = images[index].original;
-  const nextImageAlt = images[index].description;
-  lightboxImageRef.setAttribute("src", nextImageSrc);
-  lightboxImageRef.setAttribute("alt", nextImageAlt);
-}
+// function setLightboxImageRefAttribute(index) {
+//   const nextImageSrc = images[index].original;
+//   const nextImageAlt = images[index].description;
+//   lightboxImageRef.setAttribute("src", nextImageSrc);
+//   lightboxImageRef.setAttribute("alt", nextImageAlt);
+// }
   
-function onShowNextImage(event) {
-  if (!(event.code === "ArrowRight")) {
-    return;
-  }
+// function onShowNextImage(event) {
+//   if (!(event.code === "ArrowRight")) {
+//     return;
+//   }
 
-  let currentImageIndex = getIndex();
+//   let currentImageIndex = getIndex();
 
-  currentImageIndex += 1;
-   if (currentImageIndex >images.length-1) {
-     currentImageIndex = 0;
-  }
+//   currentImageIndex += 1;
+//    if (currentImageIndex >images.length-1) {
+//      currentImageIndex = 0;
+//   }
 
-  setLightboxImageRefAttribute(currentImageIndex);
+//   setLightboxImageRefAttribute(currentImageIndex);
 
-}
-function onShowPreviousImage(event) {
-if (!(event.code === "ArrowLeft")) {
-    return;
-  }
+// }
+// function onShowPreviousImage(event) {
+// if (!(event.code === "ArrowLeft")) {
+//     return;
+//   }
 
-  let currentImageIndex = getIndex();
+//   let currentImageIndex = getIndex();
 
-   if (currentImageIndex === 0) {
-     currentImageIndex = images.length;
-  } 
-  currentImageIndex -= 1;
+//    if (currentImageIndex === 0) {
+//      currentImageIndex = images.length;
+//   } 
+//   currentImageIndex -= 1;
 
-   setLightboxImageRefAttribute(currentImageIndex);
+//    setLightboxImageRefAttribute(currentImageIndex);
 
-}
+// }
 
 
-function onCloseModal() {
-  window.removeEventListener("keydown", onCloseModalByEsc);
-  lightboxRef.classList.remove("is-open");
-  onClearlightboxImageRef();
-}
+// function onCloseModal() {
+//   window.removeEventListener("keydown", onCloseModalByEsc);
+//   lightboxRef.classList.remove("is-open");
+//   onClearlightboxImageRef();
+// }
 
-function onCloseModalByEsc(event) {
-  if (event.code === "Escape") {
-    onCloseModal();
-  }
-}
+// function onCloseModalByEsc(event) {
+//   if (event.code === "Escape") {
+//     onCloseModal();
+//   }
+// }
 
-function onCloseLightboxByClick(event) {
-  if (event.currentTarget === event.target) {
-    onCloseModal();
-  }
-}
+// function onCloseLightboxByClick(event) {
+//   if (event.currentTarget === event.target) {
+//     onCloseModal();
+//   }
+// }
 
-function onClearlightboxImageRef() {
-  lightboxImageRef.removeAttribute("src");
-  lightboxImageRef.removeAttribute("alt");
-}
+// function onClearlightboxImageRef() {
+//   lightboxImageRef.removeAttribute("src");
+//   lightboxImageRef.removeAttribute("alt");
+// }
 
